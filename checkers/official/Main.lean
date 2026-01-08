d9b672b365b2ddf2233dc5daa7a7b4d7553a728b
Joachim Breitner <mail@joachim-breitner.de>
2026-01-08 11:24:33 +0000

    Parse-only checker

--- checkers/official/Main.lean
+++ checkers/official/Main.lean
@@ -13,8 +13,13 @@ def runKernel (solution : Export.ExportedEnv) : IO Unit := do
 
 
 def main (args : List String) : IO Unit := do
-  let [inputPath] := args
-    | throw <| .userError "Expected input file path as first argument."
+  let (inputPath, parseOnly) ← match args with
+    | ["--parse-only", inputPath] => pure (inputPath, true)
+    | [inputPath] => pure (inputPath, false)
+    | _ => throw <| .userError "Expected input file path as first argument, optionally followed by --parse-only."
   let content ← IO.FS.readFile inputPath
   let env ← .ofExcept (Export.parse content)
-  runKernel env
+  if parseOnly then
+    IO.println "Parse successful."
+  else
+    runKernel env
