b21dcf46f620aa76225b22f985808ab516aac578
Joachim Breitner <mail@joachim-breitner.de>
2026-01-12 11:19:26 +0000

    Manual tweaks to the tut files

--- tests/tutorial.lean
+++ tests/tutorial.lean
@@ -15,12 +15,12 @@ def tut03 : Prop := ∀ (p: Prop), p
 def tut04 : Type → Type → Type := fun x y => x
 
 -- tut05: Lambda reduction (requires two declarations)
-def tut05f : Type → Type → Type := fun x y => x
-theorem tut05thm : tut05f Prop (Prop → Prop) = Prop := rfl
+def tut05 : tut04 Prop (Prop → Prop) := ∀ p : Prop, p
 
 -- tut06: Universe polymorphism
 def tut06f.{u} : Sort u → Sort u → Sort u := fun α β => α
-theorem tut06thm : tut06f Prop (Prop → Prop) = Prop := rfl
+def tut06 : tut06f Prop (Prop → Prop) := ∀ p : Prop, p
 
 -- tut07: Function from Prop to Type (tests imax universe levels)
-def tut07 : Prop → Type := fun _ => Unit
+def tut07a (p : Prop) : Prop := Type → p
+def tut07b (α : Type) : Type 1 := Type → α
